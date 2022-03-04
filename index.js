f56175e0726f35cea1655fac354f3395cc98f14e
Vivek <vivekkumarsingh2002@gmail.com>
2022-03-05 01:46:06 +0530

    ..

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+require('dotenv').config();
+const mail_route = require('./routes/routes');
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', mail_route);
+
+app.listen(3000);
